66604d60fd076fea1a511db0375c891b703fc142
vivi <vivisangadji06@gmail.com>
2022-02-25 20:48:58 +0800

    merubah fitur edit agenda

--- application/controllers/AgendaAdmin.php
+++ application/controllers/AgendaAdmin.php
@@ -43,48 +43,28 @@ class AgendaAdmin extends CI_Controller{
 	public function editAgenda($id){
 		$data['title'] 		     = 'Ubah Agenda';
 		$data['agenda'] 	     = $this->db->get_where('agenda', ['id' => $id])->row();
+
+		if ($this->input->method() == "post") {
+			$this->updateAgenda($id);
+		}
+		$this->load->view('admin/template/header',$data); 
+		$this->load->view('admin/template/navbar'); 
+		$this->load->view('admin/agenda/editAgenda', $data); 
+		$this->load->view('admin/template/footer');
+		
+	}
+
+	public function updateAgenda($id) {
 		$this->form_validation->set_rules('nama', 'Nama', 'required');
 		$this->form_validation->set_rules('deskripsi', 'Deskripsi', 'required');
 		$this->form_validation->set_rules('waktu', 'Waktu', 'required');
-		$this->form_validation->set_rules('img', 'Gambar', 'required');
+		if(!$this->form_validation->run()) return false;
 
-		// config upload file
-		$config['upload_path']   = './public/img/agenda';
-        $config['allowed_types'] = 'jpeg|jpg|png';
-        $config['encrypt_name']  = TRUE;
-		// initialize library upload
-		$this->upload->initialize($config);
-
-        if ($this->form_validation->run() == FALSE) {
-			$this->load->view('admin/template/header',$data); 
-			$this->load->view('admin/template/navbar'); 
-			$this->load->view('admin/agenda/editAgenda', $data); 
-			$this->load->view('admin/template/footer'); 
-		}
-
-        if (!$this->upload->do_upload('img')){
-            $error = $this->upload->display_errors();
-        }else {
-        	$fileGambar = $this->upload->data();
-        	// compress image
-        	$config['image_library'] = 'gd2';
-        	$config['source_image']  = './public/img/agenda/'.$fileGambar;
-        	$config['quality']       = '50%';
-			$config['create_thumb'] = TRUE;
-	        $config['maintain_ratio']= TRUE;
-        	$config['width']         = 750;
-        	$config['height']        = 500;
-        	$config['new_image']     = './public/img/agenda/'.$fileGambar;
-        	// load library image_lib
-        	$this->load->library('image_lib',$config);
-        	$this->image_lib->resize();
-
-        	// ubah data
-        	$image = $fileGambar['file_name'];
-        	$this->mAgenda->ubahAgenda($image);
-        	$this->session->set_flashdata('alert', 'Diubah');
-        	redirect(base_url('admin/agenda'));
-        }
+		$agenda = $this->db->get_where('agenda', ['id' => $id])->row();
+		$agenda_img = $this->uploadGambar("img", $agenda->img);
+		$this->mAgenda->ubahAgenda($agenda_img);
+		$this->session->set_flashdata('alert', 'Diubah');
+		redirect(base_url('admin/agenda'));
 	}
 
 	private function uploadGambar($inputName,$gambarDefault=null){
