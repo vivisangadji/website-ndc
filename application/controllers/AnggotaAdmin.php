adda2abd76990611e4a5af58ad4c4e921d7708a8
vivi <vivisangadji06@gmail.com>
2022-02-26 18:53:52 +0800

    merubah script update anggota pada file AnggotaAdmin.php

--- application/controllers/AnggotaAdmin.php
+++ application/controllers/AnggotaAdmin.php
@@ -46,50 +46,28 @@ class AnggotaAdmin extends CI_Controller {
 	public function ubahAnggota($id){
 		$data['title']   = 'Ubah Data Anggota';
 		$data['anggota'] = $this->mAnggota->getAnggotaById($id);
+
+		if ($this->input->method() == "post") {
+			$this->updateAnggota($id);
+		} 
+		$this->load->view('admin/template/header',$data);
+		$this->load->view('admin/template/navbar');
+		$this->load->view('admin/anggota/ubahAnggota',$data);
+		$this->load->view('admin/template/footer');
+	}
+	
+	public function updateAnggota($id) {
 		$this->form_validation->set_rules('nama', 'Nama', 'required');
 		$this->form_validation->set_rules('nra', 'NRA', 'required');
 		$this->form_validation->set_rules('angkatan_ndc', 'Field ini', 'required');
 		$this->form_validation->set_rules('angkatan_kampus', 'Field ini', 'required');
 		$this->form_validation->set_rules('jabatan', 'Jabatan', 'required');
-		$this->form_validation->set_rules('gambar', 'Gambar', 'required');
-		// config upload file
-		$config['upload_path']   = './public/img/anggota';
-		$config['allowed_types'] = 'jpg|jpeg|png|gif';
-		$config['encrypt_name']  = TRUE;
-		// initialize library upload
-		$this->upload->initialize($config);
-
-		if ($this->form_validation->run() == FALSE) {
-			$this->load->view('admin/template/header',$data);
-			$this->load->view('admin/template/navbar');
-			$this->load->view('admin/anggota/ubahAnggota',$data);
-			$this->load->view('admin/template/footer');
-		} 
 		
-		if (!$this->upload->do_upload('gambar')) {
-        	$error =  $this->upload->display_errors();
-        }else {
-			$fileGambar = $this->upload->data();
-        	// compress image
-	        $config['image_library']='gd2';
-	        $config['source_image'] ='./public/img/anggota/'.$fileGambar['file_name'];
-	        $config['create_thumb'] = FALSE;
-	        $config['maintain_ratio']= TRUE;
-	        $config['quality']      = '50%';
-	        $config['width']        = 750;
-	        $config['height']       = 500;
-	        $config['new_image']    = './public/img/anggota/'.$fileGambar['file_name'];
-	        // load library image_lib
-	        $this->load->library('image_lib', $config);
-	        $this->image_lib->resize(200, 100, true, 'height');
-
-	        // insert data
-        	$data_gambar = $fileGambar['file_name'];
-            // print_r($fileGambar);
-	        $this->mAnggota->ubahAnggota($data_gambar);
-			$this->session->set_flashdata('alert','Diubah');
-			redirect(base_url('admin/anggota' ));
-		}
+		$anggota = $this->db->get_where("anggota", ["id" => $id])->row();
+		$data_gambar = $this->uploadGambar("gambar", $anggota->gambar);
+		$this->mAnggota->ubahAnggota($data_gambar);
+		$this->session->set_flashdata('alert','Diubah');
+		redirect(base_url('admin/anggota' ));
 	}
 	
 	private function uploadGambar($inputName,$gambarDefault=null){
@@ -105,6 +83,7 @@ class AnggotaAdmin extends CI_Controller {
 			$gambarDefault = $this->upload->data();
 			return $gambarDefault['file_name'];
 		}
+		return $gambarDefault;
 	}
 
 	public function hapusAnggota($id){
